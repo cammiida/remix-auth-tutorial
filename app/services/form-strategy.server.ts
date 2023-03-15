2236aef8c76a38d7cc05145e29ded24dca9dbdbc
Camilla <camillamdalan@gmail.com>
2023-03-15 19:16:53 +0100

    simplify strategy code

--- app/services/form-strategy.server.ts
+++ app/services/form-strategy.server.ts
@@ -1,21 +1,13 @@
 import { FormStrategy } from "remix-auth-form";
-import type { User } from "./auth.server";
 
-const login = async (
-  email: FormDataEntryValue | null,
-  password: FormDataEntryValue | null
-): Promise<User> => {
-  // could fetch from a database here
+export const formStrategy = new FormStrategy(async ({ form }) => {
+  let email = form.get("email");
+  let password = form.get("password");
+
   if (email === "test@test.com" && password === "test123")
     return {
       email,
       username: "testuser123",
     };
   throw new Error("Username and/or password not found.");
-};
-
-export const formStrategy = new FormStrategy(async ({ form }) => {
-  let email = form.get("email");
-  let password = form.get("password");
-  return await login(email, password);
 });
